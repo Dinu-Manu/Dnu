2dcbd74c981595e1c8206b6ebad630c19c5ddf1e
Dinu-Manu <githubdinuu@gmail.com>
2025-04-12 18:44:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ FOOTER: process.env.FOOTER || "Didula MD V2 💚",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "v642yLLA#MwhP1_qWZc_2Tzr4lns7-vGW3CDJvFx9ZDQYovUMW0g",
+SESSION_ID: process.env.SESSION_ID || "rjoByD4Y#IP-tArHzkdRysq14bVlCwBDPR4YW7o6tISMw1uWCi2o",
 OWNER_NUMBER: process.env.SESSION_ID || "94720244981",
 PREFIX: process.env.PREFIX || ".",
 ANTI_DELETE: process.env.ANTI_DELETE || "true"
